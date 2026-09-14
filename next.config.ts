db61d85005dd74f39c6e1726e342b8718adb4ba0
Marvin <marvin@MacBook-Pro-de-Marvin.local>
2026-09-14 09:05:16 -0700

    fix: update basePath to /mon-portfolio-it for GH Pages

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  ...(isProd ? { output: 'export', basePath: '/today-at-marvin' } : {}),
+  ...(isProd ? { output: 'export', basePath: '/mon-portfolio-it' } : {}),
   images: {
     unoptimized: true,
   },
